9794abf11572586def1a88346043528fb0612522
Raphael <66534805+RaphaelR89@users.noreply.github.com>
2020-09-17 11:39:42 -0300

    codigo funcional

--- nbaranking.py
+++ nbaranking.py
@@ -1,20 +1,29 @@
 # Criar uma tabela de acordo com as informações do ranking dos players da NBA stats.nba.com
-#Utilizar libs como requests2, pandas, beautifulsoup4 e selenium
-#Aprender webscraping básico.
+# Utilizar libs como requests2, pandas, beautifulsoup4 e selenium
+# Aprender webscraping básico.
 
-#Bibliotécas
+# Bibliotécas
 import time
 import requests
 import pandas as pd
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.common.exceptions import NoSuchElementException    
 import json
 
-#navegar até o site
+# navegar até o site
 url = "https://stats.nba.com/players/traditional/?SeasonType=Playoffs&sort=PTS&dir=-1"
-top10ranking= {}
+driver = webdriver.Chrome(ChromeDriverManager().install())
+driver.get(url)
+time.sleep(3)
+# ordenar por PTS
+driver.find_element_by_xpath("//button[@id='onetrust-accept-btn-handler']").click()
+# driver.find_element_by_xpath("/html/body/main/div[2]/div/div[2]/div/div/nba-stat-table/div[2]/div[1]/table/thead/tr/th[9]").click()
+element = driver.find_element_by_xpath("//div[@class='nba-stat-table']//table")
+html_content = element.get_attribute('outerHTML')
 
 rankings = {
     '3Points': {'field': 'FG3M', 'label': '3PM'},
@@ -25,44 +34,25 @@ rankings = {
     'Blocks': {'field': 'BLK', 'label': 'BLK'},
 }
 
-def buildrank(type):
-    driver = webdriver.Chrome(ChromeDriverManager().install())
-    driver.get(url)
-    #aceitar cookie
-    driver.find_element_by_xpath("//*[@id='onetrust-accept-btn-handler']").click()
-    #ordenar por PTS
-    driver.find_element_by_xpath("//div[@class='nba-stat-table']//table//thead//tr//th[@data-field='PTS']").click()
+# Parsear o conteúdo com BeautifulSoup
+soup = BeautifulSoup(html_content, 'html.parser')
+table = soup.find(name='table')
 
-    element = driver.find_element_by_xpath("//div[@class='nba-stat-table']//table")
-    html_content = element.get_attribute('outerHTML')
+# Estruturar com o Panda
+df_full = pd.read_html(str(table))[0].head(10)
+df = df_full[["Unnamed: 0", "PLAYER", "TEAM", "PTS"]]
+df.columns = ['pos', 'player', 'team', 'total']
 
-    # Parsear o conteúdo com BeautifulSoup
+# Transformar os Dados em um dicionário de dados próprio
+top10ranking = {}
+top10ranking['points'] = df.to_dict('records')
 
-    soup = BeautifulSoup(html_content, 'html.parser')
-    table = soup.find(name='table')
-
-    # Estruturar com o Panda
-
-    df_full = pd.read_html(str(table))[0].head(10)
-    df = df_full[["Unnamed: 0", "PLAYER", "TEAM", "PTS"]]
-    df.columns = ['pos', 'player', 'team', 'total']
-
-    # Transformar os Dados em um dicionário de dados próprio
-
-
-    op10ranking['points'] = df.to_dict('records')
-
-
-
-
-
-
-print(top10ranking['points'])
-
-driver.quit()
-
-#Converter e salvar em um arquivo JSON
-js=json.dumps(top10ranking)
-fp=open('ranking.json', 'w')
+# Converter e salvar em um arquivo JSON
+js = json.dumps(top10ranking)
+fp = open('ranking.json', 'w')
 fp.write(js)
 fp.close()
+
+
+
+
