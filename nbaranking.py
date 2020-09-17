7195b376a11d48d380bd0b1c8b2535f0fc366662
Raphael <66534805+RaphaelR89@users.noreply.github.com>
2020-09-17 01:48:52 -0300

    aceitar cookie

--- nbaranking.py
+++ nbaranking.py
@@ -4,7 +4,6 @@
 
 #Bibliotécas
 import time
-from pandas.io import html
 import requests
 import pandas as pd
 from bs4 import BeautifulSoup
@@ -17,9 +16,10 @@ import json
 url = "https://stats.nba.com/players/traditional/?SeasonType=Playoffs&sort=PTS&dir=-1"
 driver = webdriver.Chrome(ChromeDriverManager().install())
 driver.get(url)
-time.sleep(10)
-
-driver.find_element_by_xpath("//div[@class='nba-stat-table']//table//thead//tr//th[@data-field='PTS']").click
+#aceitar cookie
+driver.find_element_by_xpath("//*[@id='onetrust-accept-btn-handler']").click()
+#ordenar por PTS
+driver.find_element_by_xpath("//div[@class='nba-stat-table']//table//thead//tr//th[@data-field='PTS']").click()
 
 element = driver.find_element_by_xpath("//div[@class='nba-stat-table']//table")
 html_content = element.get_attribute('outerHTML')
@@ -29,15 +29,24 @@ html_content = element.get_attribute('outerHTML')
 soup = BeautifulSoup(html_content, 'html.parser')
 table = soup.find(name='table')
 
+# Estruturar com o Panda
 
+df_full = pd.read_html(str(table))[0].head(10)
+df = df_full[["Unnamed: 0", "PLAYER", "TEAM", "PTS"]]
+df.columns = ['pos', 'player', 'team', 'total']
 
+# Transformar os Dados em um dicionário de dados próprio
 
+top10ranking= {}
 
+top10ranking['points'] = df.to_dict('records')
 
+print(top10ranking['points'])
 
+# driver.quit()
 
-
-
-driver.quit()
-
-
+#Converter e salvar em um arquivo JSON
+js=json.dumps(top10ranking)
+fp=open('ranking.js', 'w')
+fp.write(js)
+fp.close()
